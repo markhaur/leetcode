660b8a6f2bc43a7f43d3e36048bcb2dfe8b525bc
Faisal Nisar <fnisar394@protonmail.com>
2023-06-09 09:52:16 +0500

    added problem description

--- climbing_stairs/main.go
+++ climbing_stairs/main.go
@@ -9,6 +9,8 @@ func main() {
 	}
 }
 
+// You are climbing a staircase. It takes n steps to reach the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 func climbStairs(n int) int {
 	var mem []int
 	mem = append(mem, 1)
